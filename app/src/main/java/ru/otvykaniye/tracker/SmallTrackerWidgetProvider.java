77a1db2d45a1e07cc32abaeadc9e0f2fb1c30c06
poorangryman <k-tarasov73@yandex.ru>
2026-08-26 14:12:47 +0400

    Publish TrackLess source: add small widget provider

---
+++ app/src/main/java/ru/otvykaniye/tracker/SmallTrackerWidgetProvider.java
@@ -0,0 +1,8 @@
+package ru.otvykaniye.tracker;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+public class SmallTrackerWidgetProvider extends AppWidgetProvider {
+ @Override public void onUpdate(Context context, AppWidgetManager manager, int[] ids){ manager.updateAppWidget(ids,WidgetUi.small(context)); }
+ @Override public void onEnabled(Context context){ WidgetUi.updateAll(context); }
+}
