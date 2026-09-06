95e9d91aa473d8b429b81c117921d4577e51e5eb
poorangryman <k-tarasov73@yandex.ru>
2026-09-06 16:11:03 +0400

    Fix Flow collection import

--- app/src/main/java/ru/otvykaniye/tracker/TracklessViewModel.kt
+++ app/src/main/java/ru/otvykaniye/tracker/TracklessViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
