1f03a6473e9e7b242579d833c5fc0d8fd5bc0772
poorangryman <317357139+poorangryman@users.noreply.github.com>
2026-09-15 23:03:42 +0400

    style: set app background to pure black and implement adaptive icons (v1.6.2)

--- app/src/main/java/ru/otvykaniye/tracker/ui/theme/Color.kt
+++ app/src/main/java/ru/otvykaniye/tracker/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package ru.otvykaniye.tracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BgDeep = Color(0xFF060B09)
+val BgDeep = Color(0xFF000000)
 val BgCard = Color(0xFF12201B)
 val GlassBorder = Color(0x14FFFFFF) // rgba(255, 255, 255, 0.08)
 val TextPrimary = Color(0xFFF1F5F9)
